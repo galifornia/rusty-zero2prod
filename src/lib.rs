ea135d219bc92e7d4e393dd63dc0ff6a5dc70907
galifornia <martinho87@gmail.com>
2023-05-12 17:51:05 +0200

    feat: randomize server port

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ async fn check_health(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: std::net::TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(check_health)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
